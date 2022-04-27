3bb45b41f5cc38c8454f391d04d3a1587ffb0df2
Light <lochois@gmail.com>
2022-04-27 16:07:30 +1000

    test var & CICD

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  cloud {
+    organization = "Devlight"
+
+    workspaces {
+      tags = ["Pylot"]
+    }
+  }
+}
+
+resource "null_resource" "example" {
+#       triggers = {
+#         value = "A example resource that does nothing!"
+#       }
+#     }
