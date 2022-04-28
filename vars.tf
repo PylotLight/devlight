5cadc93ef05df64cbe16263b0287e90dfc1c0622
Light <lochois@gmail.com>
2022-04-28 19:54:46 +1000

    gcm update vars

--- vars.tf
+++ vars.tf
@@ -9,24 +9,24 @@ variable "azure_region" {
 }
 
 variable "AZURE_CLIENT_ID" {
-  description = "AZURE_CLIENT_ID"
+  description = "AZURE CLIENT ID"
   type        = string
   sensitive   = true
 }
 
 variable "AZURE_CLIENT_SECRET" {
-  description = "AZURE_CLIENT_SECRET"
+  description = "AZURE CLIENT SECRET"
   type        = string
   sensitive   = true
 }
 
 variable "AZURE_SUBSCRIPTION_ID" {
-  description = "AZURE_SUBSCRIPTION_ID"
+  description = "AZURE SUBSCRIPTION ID"
   type        = string
   sensitive   = true
 }
 variable "AZURE_TENANT_ID" {
-  description = "AZURE_TENANT_ID"
+  description = "AZURE TENANT ID"
   type        = string
   sensitive   = true
 }
