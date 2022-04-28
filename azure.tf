7b6e261f2127460b50f0ca96f6accad9679c2364
Light <lochois@gmail.com>
2022-04-28 18:29:01 +1000

    gcm workflow update

--- azure.tf
+++ azure.tf
@@ -11,67 +11,67 @@ resource "azurerm_resource_group" "azure_rg" {
   location = var.azure_region
 }
 
-resource "azurerm_monitor_action_group" "azure_mag" {
-  name                = "Monitor-Action-Group"
-  resource_group_name = azurerm_resource_group.azure_rg.name
-  short_name          = "MAG"
-}
+# resource "azurerm_monitor_action_group" "azure_mag" {
+#   name                = "Monitor-Action-Group"
+#   resource_group_name = azurerm_resource_group.azure_rg.name
+#   short_name          = "MAG"
+# }
 
-resource "azurerm_consumption_budget_subscription" "azure_bs" {
-  name            = "Budget-Subscription"
-  subscription_id = data.azurerm_subscription.current.id
+# resource "azurerm_consumption_budget_subscription" "azure_bs" {
+#   name            = "Budget-Subscription"
+#   subscription_id = data.azurerm_subscription.current.id
 
-  amount     = 1
-  time_grain = "Monthly"
+#   amount     = 1
+#   time_grain = "Monthly"
 
-  time_period {
-    start_date = "2022-04-01T00:00:00Z"
-  }
+#   time_period {
+#     start_date = "2022-04-01T00:00:00Z"
+#   }
 
-  filter {
-    dimension {
-      name = "ResourceGroupName"
-      values = [
-        azurerm_resource_group.azure_rg.name,
-      ]
-    }
+#   filter {
+#     dimension {
+#       name = "ResourceGroupName"
+#       values = [
+#         azurerm_resource_group.azure_rg.name,
+#       ]
+#     }
 
-    tag {
-      name = "foo"
-      values = [
-        "bar",
-        "baz",
-      ]
-    }
-  }
+#     tag {
+#       name = "foo"
+#       values = [
+#         "bar",
+#         "baz",
+#       ]
+#     }
+#   }
 
-  notification {
-    enabled   = true
-    threshold = 0
-    operator  = "GreaterThan"
+#   notification {
+#     enabled   = true
+#     threshold = 0
+#     operator  = "GreaterThan"
 
-    contact_emails = [
-      "foo@example.com",
-      "bar@example.com",
-    ]
+#     contact_emails = [
+#       "foo@example.com",
+#       "bar@example.com",
+#     ]
 
-    contact_groups = [
-      azurerm_monitor_action_group.azure_mag.id,
-    ]
+#     contact_groups = [
+#       azurerm_monitor_action_group.azure_mag.id,
+#     ]
 
-    contact_roles = [
-      "Owner",
-    ]
-  }
+#     contact_roles = [
+#       "Owner",
+#     ]
+#   }
 
-  notification {
-    enabled        = true
-    threshold      = 1.0
-    operator       = "GreaterThan"
-    threshold_type = "Actual"
+#   notification {
+#     enabled        = true
+#     threshold      = 1.0
+#     operator       = "GreaterThan"
+#     threshold_type = "Actual"
 
-    contact_emails = [
-      ""
-    ]
-  }
-}
+#     contact_emails = [
+#       ""
+#     ]
+#   }
+# }
