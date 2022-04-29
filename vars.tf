a29f10ee82a5238ed97cda0dfa3919da4b8474c5
Light <lochois@gmail.com>
2022-04-29 15:37:13 +1000

    Awssetup (#13)
    * gcm aws init
    
    * gcm formatting
    
    * gcm file formatting
    
    * gcm formatting 2
    
    * gcm formatting 3

--- vars.tf
+++ vars.tf
@@ -1,8 +1,22 @@
+#AWS
 variable "aws_region" {
   description = "AWS Region"
   default     = "ap-southeast-2"
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS ACCESS KEY ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS SECRET ACCESS KEY"
+  type        = string
+  sensitive   = true
+}
+
+#Azure
 variable "azure_region" {
   description = "Azure Region"
   default     = "australiasoutheast"
@@ -30,4 +44,3 @@ variable "ARM_TENANT_ID" {
   type        = string
   sensitive   = true
 }
-
