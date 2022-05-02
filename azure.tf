fc98aee0d816eaea18f02532f0a10d8aa39aa008
Light <lochois@gmail.com>
2022-05-02 12:16:12 +1000

    gcm add tags to mag

--- azure.tf
+++ azure.tf
@@ -20,6 +20,7 @@ resource "azurerm_monitor_action_group" "azure_mag" {
   name                = "Monitor-Action-Group"
   resource_group_name = azurerm_resource_group.azure_rg.name
   short_name          = "MAG"
+  tags                = {}
 }
 
 # resource "azurerm_consumption_budget_subscription" "azure_bs" {
