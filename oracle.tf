0689b44991a22fc3c0d4000f340ed77b15125a23
Light <lochois@gmail.com>
2022-05-02 19:23:02 +1000

    gcm formatting oracle.tf

--- oracle.tf
+++ oracle.tf
@@ -16,12 +16,12 @@ resource "oci_core_vcn" "internal" {
 }
 
 resource "oci_core_subnet" "dev" {
-  vcn_id                      = oci_core_vcn.internal.id
-  cidr_block                  = "172.16.0.0/24"
-  compartment_id              = var.OCI_tenancy_ocid
-  display_name                = "Core Subnet"
-  prohibit_public_ip_on_vnic  = true
-  dns_label                   = "Core"
+  vcn_id                     = oci_core_vcn.internal.id
+  cidr_block                 = "172.16.0.0/24"
+  compartment_id             = var.OCI_tenancy_ocid
+  display_name               = "Core Subnet"
+  prohibit_public_ip_on_vnic = true
+  dns_label                  = "Core"
 }
 
 # # Get a list of Availability Domains
