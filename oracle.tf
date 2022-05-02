45105d4136f13dab4fc70a973d419797cb251c2b
Light <lochois@gmail.com>
2022-05-02 20:55:11 +1000

    gcm remove whitespaces

--- oracle.tf
+++ oracle.tf
@@ -5,8 +5,8 @@ provider "oci" {
   user_ocid        = var.OCI_user_ocid
   private_key      = var.OCI_rsa_private_key
   private_key_path = ""
-  fingerprint       = var.OCI_fingerprint
-  region            = var.OCI_region_identifier
+  fingerprint      = var.OCI_fingerprint
+  region           = var.OCI_region_identifier
 }
 
 resource "oci_core_vcn" "internal" {
