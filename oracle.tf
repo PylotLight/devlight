f3fdda589bc3393da878cf57716b5eb9a2112ad2
Light <lochois@gmail.com>
2022-05-02 18:20:20 +1000

    gcm add vcn config

--- oracle.tf
+++ oracle.tf
@@ -8,6 +8,13 @@ provider "oci" {
   region       = var.OCI_region_identifier
 }
 
+resource "oci_core_vcn" "internal" {
+  dns_label      = "internal"
+  cidr_block     = "172.16.0.0/20"
+  compartment_id = var.OCI_tenancy_ocid
+  display_name   = "Internal VCN"
+}
+
 # # Get a list of Availability Domains
 # data "oci_identity_availability_domains" "ads" {
 #   compartment_id = var.tenancy_ocid
