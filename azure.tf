314cf957f9b61b9a365a93fade53afddfdc8e1a6
Light <lochois@gmail.com>
2022-05-02 12:10:29 +1000

    gcm fix apply file and az tags (#17)
    * gcm fix apply file and az tags
    
    * gcm tags formatting fix

--- azure.tf
+++ azure.tf
@@ -13,6 +13,7 @@ data "azurerm_subscription" "current" {}
 resource "azurerm_resource_group" "azure_rg" {
   name     = "DevLight-Resource-Group"
   location = var.azure_region
+  tags     = {}
 }
 
 resource "azurerm_monitor_action_group" "azure_mag" {
