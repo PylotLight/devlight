4938697847542f3afe7012f9351d561164fe5180
Light <lochois@gmail.com>
2022-05-13 15:52:03 +1000

    gcm azure json auth

--- azure.tf
+++ azure.tf
@@ -2,10 +2,11 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
+  # subscription_id             = var.ARM_SUBSCRIPTION_ID
+  # client_id                   = var.ARM_CLIENT_ID
+  # client_certificate_path     = var.ARM_client_certificate_path
+  # client_certificate_password = var.ARM_client_certificate_password
+  # tenant_id                   = var.ARM_TENANT_ID
 }
 
 data "azurerm_subscription" "current" {}
