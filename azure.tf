0f0bba0edfccb1479cb3e787e1d6283bc0437f86
Lochy <lochois@gmail.com>
2022-05-14 19:28:31 +1000

    gcm allow deletion

--- azure.tf
+++ azure.tf
@@ -6,6 +6,9 @@ provider "azurerm" {
   client_id       = var.ARM_CLIENT_ID
   client_secret   = var.ARM_CLIENT_SECRET
   tenant_id       = var.ARM_TENANT_ID
+  resource_group {
+│       prevent_deletion_if_contains_resources = false
+│     }
 }
 
 data "azurerm_subscription" "current" {}
