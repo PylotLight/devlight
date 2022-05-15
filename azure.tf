ab8423ef46da8e0347669c63ca987b57d9d504c6
Lochy <lochois@gmail.com>
2022-05-15 14:40:26 +1000

    gcm remove storage info

--- azure.tf
+++ azure.tf
@@ -86,24 +86,24 @@ resource "azurerm_network_interface_security_group_association" "az_nisga" {
   network_security_group_id = azurerm_network_security_group.az_secgp.id
 }
 
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.azure_rg.name
-  }
+# # Generate random text for a unique storage account name
+# resource "random_id" "randomId" {
+#   keepers = {
+#     # Generate a new ID only when a new resource group is defined
+#     resource_group = azurerm_resource_group.azure_rg.name
+#   }
 
-  byte_length = 8
-}
+#   byte_length = 8
+# }
 
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "az_storageacc" {
-  name                     = "diag${random_id.randomId.hex}"
-  location                 = azurerm_resource_group.azure_rg.location
-  resource_group_name      = azurerm_resource_group.azure_rg.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+# # Create storage account for boot diagnostics
+# resource "azurerm_storage_account" "az_storageacc" {
+#   name                     = "diag${random_id.randomId.hex}"
+#   location                 = azurerm_resource_group.azure_rg.location
+#   resource_group_name      = azurerm_resource_group.azure_rg.name
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
 
 # Create (and display) an SSH key
 resource "tls_private_key" "az_ssh_key" {
@@ -135,9 +135,9 @@ resource "azurerm_linux_virtual_machine" "az_linuxvm" {
     username   = "azureuser"
     public_key = tls_private_key.az_ssh_key.public_key_openssh
   }
-  boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.az_storageacc.primary_blob_endpoint
-  }
+  # boot_diagnostics {
+  #   storage_account_uri = azurerm_storage_account.az_storageacc.primary_blob_endpoint
+  # }
 }
 
 # resource "azurerm_consumption_budget_subscription" "azure_bs" {
