6227424e11f926d212a49df7ec379a9650cf1000
Lochy <lochois@gmail.com>
2022-05-16 20:05:54 +1000

    gcm formatting fit

--- aws.tf
+++ aws.tf
@@ -8,11 +8,11 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "vpc" {
   cidr_block = "172.16.0.0/16"
-}
+
 
 resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "172.16.10.0/24"
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "172.16.10.0/24"
 }
 
 resource "aws_network_interface" "nic" {
